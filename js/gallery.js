642a078f0610e06a818ba6114be54005148f675f
Pearce <pearcem.dx@gmail.com>
2022-05-30 09:31:05 -0400

    actually set site to Binary Nomad Games

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    console.log('document ready');
+    $('#debris-field-gallery').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
