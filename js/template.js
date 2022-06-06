7954a9a0d3a55716cf62294753ac2b11eb0ad785
Pearce <pearcem.dx@gmail.com>
2022-06-06 12:23:32 -0400

    add debug stuff

--- js/template.js
+++ js/template.js
@@ -2,10 +2,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   // get current file name
   var url = window.location.pathname;
+  console.log(url);
+
   var file = url.substring(url.lastIndexOf('/')+1);
+  console.log(file);
 
   // Remove the extension
   var filename = file.substring(0, file.lastIndexOf('.')); 
+  console.log(filename);
 
   fetch("./templates/header.html")
     .then((response) => response.text())
