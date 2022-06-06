1926f50f211ce4a4ad2a959301bb5ceb82d223ca
Pearce <pearcem.dx@gmail.com>
2022-06-06 11:29:37 -0400

    fetch header

--- js/template.js
+++ js/template.js
@@ -1,5 +1,14 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
+  fetch("./templates/header.html" /*, options */)
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById("header").innerHTML = html;
+    })
+    .catch((error) => {
+        console.warn(error);
+  });
+
   fetch("./templates/footer.html" /*, options */)
     .then((response) => response.text())
     .then((html) => {
