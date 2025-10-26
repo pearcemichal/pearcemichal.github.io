7e3705dec264c9947475da4e6d087531217dda23
TeslaCuil <pearcem.dx@gmail.com>
2025-10-26 16:38:45 -0400

    add support for multiple games, add fight glub playable web build, update contact and year

--- js/template.js
+++ js/template.js
@@ -1,32 +1,32 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-
+window.addEventListener("DOMContentLoaded", (event) => {
   // get current file name
   var url = window.location.pathname;
-  var file = url.substring(url.lastIndexOf('/')+1);
+  var file = url.substring(url.lastIndexOf("/") + 1);
 
   // Remove the extension
-  var filename = file.substring(0, file.lastIndexOf('.')); 
+  var filename = file.substring(0, file.lastIndexOf("."));
 
   fetch("/templates/header.html")
     .then((response) => response.text())
     .then((html) => {
-        document.getElementById("header").innerHTML = html;
+      document.getElementById("header").innerHTML = html;
 
-        var link = document.getElementById("" + filename);
-        link.classList.add("current-page");
+      var link = document.getElementById("" + filename);
+      link.classList.add("current-page");
     })
     .catch((error) => {
-        console.warn(error);
-  });
+      console.warn(error);
+    });
 
   fetch("/templates/footer.html")
     .then((response) => response.text())
     .then((html) => {
-        document.getElementById("footer").innerHTML = html;
+      document.getElementById("footer").innerHTML = html;
+
+      document.getElementById("legal-year").innerHTML =
+        new Date().getFullYear();
     })
     .catch((error) => {
-        console.warn(error);
-  });
-
+      console.warn(error);
+    });
 });
-
